6d6b6c94860380ea4a21102d88e9c01006d1aee8
Will Hedges <wshedges@outlook.com>
2022-12-13 12:06:22 -0600

    service form now saving to permanent state

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -16,3 +16,17 @@ export const fetchRequests = () => {
 export const getRequests = () => {
     return applicationState.requests.map((req) => ({ ...req }));
 };
+
+export const sendRequest = (userServiceRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userServiceRequest),
+    };
+
+    return fetch(`${API}/requests`, fetchOptions)
+        .then((response) => response.json())
+        .then(() => {});
+};
